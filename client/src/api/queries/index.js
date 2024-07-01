2e801ccb1bf4f872ebc23fc2def97df67fe218f9
kz0ki <momotenko536@gmail.com>
2024-07-01 16:57:38 +0300

    fix queries

--- client/src/api/queries/index.js
+++ client/src/api/queries/index.js
@@ -2,14 +2,15 @@ import axios from "axios";
 import moment from "moment";
 import * as API_URL from "../index.js";
 
+const getHeaders = () => ({
+  Authorization: `Token ${JSON.parse(localStorage.getItem("token"))?.token}`,
+});
 export const getMe = (token) => {
   try {
     const response = axios({
       method: "get",
       url: API_URL.TOKEN_URL,
-      headers: {
-        Authorization: `Token ${token}`,
-      },
+      headers: getHeaders(),
     });
     return response ? response : null;
   } catch (error) {
@@ -21,7 +22,7 @@ export const getUsers = () => {
   const response = axios({
     method: "get",
     url: API_URL.USERS,
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
   });
   return response;
 };
@@ -30,7 +31,7 @@ export const getUser = (userId) => {
   const response = axios({
     method: "get",
     url: API_URL.USERS + userId + "/",
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
   });
   return response;
 };
@@ -44,7 +45,7 @@ export const logout = async () => {
   const response = await axios({
     method: "post",
     url: API_URL.LOG_OUT,
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
   });
   return response;
 };
@@ -72,8 +73,7 @@ export const resetPassword = async (uid, token, values) => {
   return response;
 };
 export const signUp = async (values) => {
-  const response = await axios.post(API_URL.REGISTER, { ...values });
-  return response;
+  return await axios.post(API_URL.REGISTER, { ...values });
 };
 
 export const editProfile = async (values) => {
@@ -81,9 +81,7 @@ export const editProfile = async (values) => {
     const response = await axios({
       method: "put",
       url: API_URL.TOKEN_URL,
-      headers: {
-        Authorization: `Token ${localStorage.getItem("token")}`,
-      },
+      headers: getHeaders(),
       data: {
         username: values.username,
         email: values.email,
@@ -96,9 +94,7 @@ export const editProfile = async (values) => {
     const response = await axios({
       method: "put",
       url: API_URL.TOKEN_URL,
-      headers: {
-        Authorization: `Token ${localStorage.getItem("token")}`,
-      },
+      headers: getHeaders(),
       data: {
         username: values.username,
         email: values.email,
@@ -114,14 +110,14 @@ export const getTask = async (all = false, page = null) => {
     const response = await axios({
       method: "get",
       url: API_URL.GET_ALL_TASKS,
-      headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+      headers: getHeaders(),
     });
     return response;
   } else if (page !== null) {
     const response = await axios({
       method: "get",
       url: API_URL.PAGINATE_TASKS + page,
-      headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+      headers: getHeaders(),
     });
     return response;
   } else {
@@ -133,7 +129,7 @@ export const createTask = async (values) => {
   const response = await axios({
     method: "post",
     url: API_URL.CRUD_TASKS,
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
     data: {
       name: values.title,
       description: values.description,
@@ -150,7 +146,7 @@ export const updateTask = async (taskId, values) => {
   const response = await axios({
     method: "put",
     url: API_URL.CRUD_TASKS + taskId + "/",
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
     data: {
       name: values.title,
       description: values.description,
@@ -167,7 +163,7 @@ export const deleteTask = async (taskId) => {
   const response = await axios({
     method: "delete",
     url: API_URL.CRUD_TASKS + taskId + "/",
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
     data: {
       auth_token: localStorage.getItem("token"),
     },
@@ -179,7 +175,7 @@ export const verifyToken = async () => {
   const response = await axios({
     method: "get",
     url: API_URL.VERIFY_TOKEN,
-    headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+    headers: getHeaders(),
   });
   return response;
 };
